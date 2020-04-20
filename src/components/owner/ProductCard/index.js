2deea13a06c448b0e8a08d8b06ad26d127dd76d9
megashrieks <shrikanthbudya@gmail.com>
2020-04-21 00:21:49 +0530

    added product card component some style changes

---
+++ src/components/owner/ProductCard/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Lens, LensOutlined} from "@material-ui/icons"
+import './ProductCard.css'
+import { Link } from 'react-router-dom';
+export default ({ name, imageURL, price, description, rating, id }) => {
+    return <Link className="product-card" to={"owner/"+id}>
+        <div className="product-image">
+            <img src={imageURL} />
+        </div>
+        <div className="details">
+            <div className="flex">
+                <div>
+                    <div className="greyed">name</div>
+                    <div className="name">{name}</div>
+                </div>
+                <div>
+                    <div className="greyed">ratings</div>
+                    <div className="ratings">{new Array(5).fill(0).map((_, index) => {
+                        if (index+1 <= rating) return <Lens fontSize="inherit"/>
+                        return <LensOutlined fontSize="inherit"/>
+                    })}</div>
+                </div>
+            </div>
+
+            <div>
+                <div className="greyed">description</div>
+                <div className="description">{description.substr(0, 60) + "..."}</div>
+            </div>
+            <div>
+                <div className="greyed">price</div>
+                <div className="price"><span className="greyed" style={{ color: "black" }}>$</span>{price}</div>
+            </div>
+        </div>
+    </Link>
+}
--- src/components/owner/dashboard.js
+++ src/components/owner/dashboard.js
@@ -1,6 +1,23 @@
 import React from "react";
 import './dashboard.css'
-import Collapsible from "../HOC/Collapsible";
+import Collapsible from "../HOC/collapsible";
+import ProductCard from "./ProductCard";
+let standard_description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+let standard_imageURL = "https://image.shutterstock.com/image-vector/concept-touch-screen-smartphone-blank-600w-1433190200.jpg";
+
+let dummy = [
+	{
+		id:"_",
+		name: "Phone",
+		price: 17000,
+		rating: 3.5,
+		description: standard_description,
+		imageURL:standard_imageURL
+	}
+]
+dummy.push(...dummy)
+dummy.push(...dummy)
+dummy.push(...dummy)
 function Dashboard() {
 	return (
 		<div className="full-container h-padd">
@@ -8,7 +25,13 @@ function Dashboard() {
 				<h3>Welcome ShopOwner</h3>
 			</div>
 			<Collapsible title="My Products">
-				Product 1
+				<div style={{
+					"display": "flex",
+					"justifyContent": "space-evenly",
+					"flexWrap":"wrap"
+				}}>
+					{dummy.map((item, index) => <ProductCard {...item} key={index} />)}
+				</div>
 			</Collapsible>
 		</div>
 	);
