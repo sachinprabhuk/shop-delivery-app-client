b5a8115efb90a4bf82224d5976bc6295480e0d29
TheMundaneGuy <55886111+TheMundaneGuy@users.noreply.github.com>
2020-05-17 13:36:10 +0530

    Update Home.js
    Fetches ML Data in allitemdata

--- src/components/app/Home.js
+++ src/components/app/Home.js
@@ -1,11 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import * as firebase from 'firebase'
 import { Container } from "react-bootstrap";
 import { withStdTopNav, withStdBottomNav } from "../HOC/NavHOC";
+import { db } from "../../firebase";
+
 
 export const Home = withStdBottomNav(withStdTopNav(() => {
+    
+    const [userdoc,setDoc] = useState("");
+    const [loading,setLoading] = useState(true);
+    // const []
+
+        
+    var email=firebase.auth().currentUser.email;
+    var items ={};
+    var allitems = [];
+    var allitemdata ={};
+    const fetchData = async() => {
+        await db.collection("Users").where("email","==",email).get().then((snapshot)=>{
+            snapshot.forEach((snap)=>{
+                setDoc(snap.id);
+                return;     
+            }) 
+        }).then(()=>{
+            return db.collection("Users").doc(userdoc).collection("ML").doc("Items").get()
+        }).then((snapshot)=>{
+            items = snapshot.data();
+            var itemkeys =Object.keys(items);
+            return db.collection("Items").where(firebase.firestore.FieldPath.documentId(), "in", itemkeys).get(); 
+        
+        }).then((snapshot)=>{
+            snapshot.forEach((snaps)=>{
+                allitems.push(snaps.id)
+                allitemdata[snaps.id]= snaps.data();
+            })
+            setLoading(false);
+        }).catch((err)=>{
+            
+        }) 
+        
+        if(!loading)
+            document.getElementById("demo").innerHTML =JSON.stringify(allitemdata);
+    }
+    fetchData();
     return (
         <Container>
-            <p>Recommendations will be here</p>
+            <p id="demo">Loading...</p>
         </Container>
     );
 }));
