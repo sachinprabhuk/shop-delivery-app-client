2733b5b75a91ca464571495da3cc2f21b3bd44f6
schkn <schkn@amazon.com>
2020-05-24 12:54:36 +0530

    added cart page logic, ui needs to be done

--- src/components/app/Cart.js
+++ src/components/app/Cart.js
@@ -1,11 +1,49 @@
-import React from "react";
+import React, { useContext, useCallback } from "react";
 import { Container } from "react-bootstrap";
-import { withGoBackTopNav} from "../HOC/NavHOC";
+import { withGoBackTopNav } from "../HOC/NavHOC";
+import { useAsync } from "../../hooks/useAsync";
+import { db } from "../../firebase";
+import { AuthContext } from "../../contexts/AuthContext";
+import { CART_COLLECTION } from "../../constants/constants";
+import { Loader } from "../utils/Loader";
 
 export const Cart = withGoBackTopNav(() => {
-    return (
-        <Container>
-            <p>Cart items will be displayed here</p>
-        </Container>
-    );
+    const {
+        user: { uid },
+    } = useContext(AuthContext);
+
+    const fetchCartItems = useCallback(async () => {
+        const raw = await db.collection(CART_COLLECTION).where("userID", "==", uid).get();
+        if (!raw || raw.docs.length === 0) {
+            throw new Error();
+        } else {
+            return raw.docs.map((el) => {
+                const doc = el.data();
+                doc.id = el.id;
+                return doc;
+            });
+        }
+    }, [uid]);
+
+    const { error, data } = useAsync(fetchCartItems);
+
+    let toRender = <Loader fullPage message="fetching cart items..." />;
+    if (error) {
+        toRender = <p>{error}</p>;
+    } else if (data) {
+        console.log(data);
+        toRender = (
+            <ul>
+                {data.map((el) => {
+                    return (
+                        <li key={el.id}>
+                            {el.product.name} - {el.shop.name} - {el.price}
+                        </li>
+                    );
+                })}
+            </ul>
+        );
+    }
+
+    return <Container>{toRender}</Container>;
 });
---
+++ src/hooks/useAsync.js
@@ -0,0 +1,42 @@
+import { useEffect, useReducer, useCallback } from "react";
+import { START_FETCH, FAILURE_FETCH, SUCCESS_FETCH } from "../constants/constants";
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case START_FETCH:
+            return { ...state, data: null, fetching: true, error: null };
+        case SUCCESS_FETCH:
+            return { ...state, data: action.payload, error: null, fetching: false };
+        case FAILURE_FETCH:
+            return { ...state, data: null, error: action.payload, fetching: false };
+        default:
+            throw new Error("Invalid action type");
+    }
+};
+
+export const useAsync = (cb, fetchImmediate = true) => {
+    const [{ data, fetching, error }, dispatch] = useReducer(reducer, {
+        data: null,
+        fetching: true,
+        error: null,
+    });
+
+    const fetchFn = useCallback(async () => {
+        try {
+            console.log("calling useAsync fetch....");
+            dispatch({ type: START_FETCH });
+            const data = await cb();
+            dispatch({ type: SUCCESS_FETCH, payload: data });
+        } catch (e) {
+            dispatch({ type: FAILURE_FETCH, payload: "error while fetching data" });
+        }
+    }, [cb]);
+
+    useEffect(() => {
+        if (fetchImmediate) {
+            fetchFn();
+        }
+    }, [cb, fetchFn, fetchImmediate]);
+
+    return { fetching, error, data, fetchFn };
+};
