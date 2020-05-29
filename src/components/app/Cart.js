faa074609f2eb77947fc1242bc2aac349b57ce6b
Sheldon <sheldon@chalkstreet.com>
2020-05-29 11:20:36 +0530

    Home page and Cart Page

--- src/components/app/Cart.js
+++ src/components/app/Cart.js
@@ -1,11 +1,123 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Button } from "react-bootstrap";
 import { withGoBackTopNav} from "../HOC/NavHOC";
 
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+
+
+const useStyles = makeStyles({
+    main: {
+        boxShadow:"0px 2px 9px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12)"
+    },
+    root: {
+      display: 'flex',
+      borderRadius:"0px",
+    },
+    details: {
+      display: 'flex',
+      flexDirection: 'column',
+    },
+    content: {
+      flex: '1 0 auto',
+    },
+    cover: {
+      width: 151,
+      margin:"10px"
+    },
+});
+
 export const Cart = withGoBackTopNav(() => {
-    return (
-        <Container>
-            <p>Cart items will be displayed here</p>
-        </Container>
-    );
+    const {
+        user: { uid },
+    } = useContext(AuthContext);
+    const classes = useStyles();
+    const fetchCartItems = useCallback(async () => {
+        const raw = await db.collection(CART_COLLECTION).where("userID", "==", uid).get();
+        if (!raw || raw.docs.length === 0) {
+            throw new Error();
+        } else {
+            const cartItems = raw.docs.map((el) => {
+                const doc = el.data();
+                doc.id = el.id;
+                return doc;
+            });
+            console.log("cart items -> ", cartItems);
+            return cartItems;
+        }
+    }, [uid]);
+
+    const { error, data } = useAsync(fetchCartItems);
+
+    let toRender = <Loader fullPage message="fetching cart items..." />;
+    if (error) {
+        toRender = <p>{error}</p>;
+    } else if (data) {
+        toRender = (
+            <>
+                <div  >
+                    {data.map((el) => {
+                        return (
+                            <div key={el.id} className="" style={{marginBottom:"40px"}}>
+                            <Card className={classes.main} >
+                                <div className={classes.root}> 
+                                <CardMedia
+                                    className={classes.cover}
+                                    image={el.product.image}
+                                    title="Live from space album cover"
+                                />
+                                
+                                <CardActionArea>
+                                <div className={classes.details}>
+                                    <CardContent className={classes.content}>
+
+                                    <div style={{ }} className="float-right d-flex">
+                                        <Button style={{ border:"none", marginRight:"3px"}} variant="light">+</Button>
+                                            <label style={{width:"30px", textAlign:"center", marginTop:"4px"}}>2</label>
+                                        <Button style={{ border:"none", marginLeft:"3px"}}variant="light">-</Button>
+                                    </div>
+                                    <h5>{el.product.name}</h5>
+                               
+                                
+                                 <Typography variant="body1" color="textSecondary" component="p">
+                                 
+                                    Price : ₹{el.price} <br />
+                                    {el.shop.name} 
+                                    
+                                    </Typography>
+                                    </CardContent>
+                                </div>
+                                </CardActionArea>
+                                <br />
+                                </div>
+                               
+                                <Button style={{borderRadius:"0px", width:"100%", border:"none"}} size="lg" variant="outline-danger">Remove</Button>  
+                               
+                            </Card>
+                           
+                            </div>
+                        );
+                    })}
+                    
+                </div>
+                
+                
+                    <Col>
+                    <h5>Grand Total  :  ₹ {data.reduce((acc, curr) => acc + Number.parseFloat(curr.price), 0)} </h5>
+                    </Col>
+                    <br />
+                    <Col>
+                        <Button className="float-right" variant="warning">Place order</Button>
+                    </Col>
+               
+            </>
+        );
+    }
+
+    return <Container><br /><h5>Items</h5><br />{toRender}<br /><br /><br /></Container>;
 });
--- src/components/app/Home.js
+++ src/components/app/Home.js
@@ -2,10 +2,106 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import { withStdTopNav, withStdBottomNav } from "../HOC/NavHOC";
 
-export const Home = withStdBottomNav(withStdTopNav(() => {
-    return (
-        <Container>
-            <p>Recommendations will be here</p>
-        </Container>
-    );
-}));
+import { makeStyles,  useTheme } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+;
+
+const useStyles = makeStyles({
+    root: {
+      display: 'flex',
+      marginBottom: "40px",
+      borderRadius:"0px",
+      boxShadow:"0px 2px 9px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12)"
+    },
+    details: {
+      display: 'flex',
+      flexDirection: 'column',
+    },
+    content: {
+      flex: '1 0 auto',
+    },
+    cover: {
+    margin:20,
+      width: 82,
+      height:80
+    
+    },
+  });
+
+export const Home = withStdBottomNav(
+    withStdTopNav(() => {
+        const {
+            user: { uid },
+        } = useContext(AuthContext);
+        const classes = useStyles();
+        const fetchRecommendations = useCallback(async () => {
+            const rawRecommendationDoc = await db.doc(`${USER_COLLECTION}/${uid}/ML/Items`).get();
+            const recommendationDoc = rawRecommendationDoc.data();
+            const sortedData = Object.entries(recommendationDoc).sort((a, b) => b[1] - a[1]);
+            const recommendationPromises = sortedData.map(([itemID]) => {
+                return db.doc(`${ITEMS_COLLECTION}/${itemID}`).get();
+            });
+            const recommendationsRaw = await Promise.all(recommendationPromises);
+            const recommendations = recommendationsRaw.map((el) => {
+                const doc = el.data();
+                doc.id = el.id;
+                return doc;
+            });
+            console.log("Recommendations -> ", recommendations);
+            return recommendations;
+        }, [uid]);
+
+        const { data, error } = useAsync(fetchRecommendations);
+
+        let toRender = <Loader fullPage message="loading recommendations..." />;
+        if (error) {
+            toRender = <p>{error}</p>;
+        } else if (data) {
+            toRender = (
+                <div>
+                    {data.map((el) => {
+                        return (
+
+                            <Card className={classes.root} key={el.id}>
+                                
+                                
+                                <CardActionArea>
+                                <div className={classes.details}>
+                                    <CardContent className={classes.content}>
+                                    <h5>{el.name}</h5>
+                                    <Typography variant="body1" color="textSecondary" component="p">
+                                     {el.company} <br />
+                                     {el.origin} <br />
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary" component="p">                                  
+                                   {el.details} <br />
+                                    </Typography>
+                                    </CardContent>
+                                </div>
+                                </CardActionArea>
+                                <div style={{textAlign:"center"}}>
+                                <CardMedia
+                                    className={classes.cover}
+                                    image={el.image}
+                                    component="img"
+                                    title="Live from space album cover"
+                                />
+                                 <h5 style={{fontSize:"17px"}}> Price : ₹ {el.price}/{el.unit} </h5>    
+                                </div>
+                                
+                            </Card>
+                          
+                        );
+                    })}
+                </div>
+           
+            );
+        }
+
+        return <Container ><br /><h5>Recommendations</h5><br />{toRender}</Container>;
+    })
+);
